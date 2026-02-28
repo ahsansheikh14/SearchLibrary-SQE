d9c6219795fdce0e137da5720d4ad897ed2934e6
ahsansheikh14 <146324815+ahsansheikh14@users.noreply.github.com>
2026-02-28 18:42:40 +0500

    github workflow

--- SearchLibrary.Tests/BinarySearchTests.cs
+++ SearchLibrary.Tests/BinarySearchTests.cs
@@ -4,35 +4,58 @@ using SearchLibrary;
 public class BinarySearchTests
 {
     [Fact]
-    public void Search_Found()
+    public void Search_ElementFound()
     {
         var bs = new BinarySearch();
         int[] arr = {1,2,3,4,5};
-
-        int result = bs.Search(3, arr);
-
-        Assert.Equal(2, result);
+        Assert.Equal(2, bs.Search(3, arr));
     }
 
     [Fact]
-    public void Search_NotFound()
+    public void Search_ElementNotFound()
     {
         var bs = new BinarySearch();
         int[] arr = {1,2,3,4,5};
-
-        int result = bs.Search(10, arr);
-
-        Assert.Equal(-1, result);
+        Assert.Equal(-1, bs.Search(10, arr));
     }
 
     [Fact]
     public void Search_EmptyArray()
     {
         var bs = new BinarySearch();
-        int[] arr = { };
+        int[] arr = {};
+        Assert.Equal(-1, bs.Search(3, arr));
+    }
 
-        int result = bs.Search(3, arr);
+    [Fact]
+    public void Search_SingleElement_Found()
+    {
+        var bs = new BinarySearch();
+        int[] arr = {7};
+        Assert.Equal(0, bs.Search(7, arr));
+    }
 
-        Assert.Equal(-1, result);
+    [Fact]
+    public void Search_SingleElement_NotFound()
+    {
+        var bs = new BinarySearch();
+        int[] arr = {7};
+        Assert.Equal(-1, bs.Search(5, arr));
+    }
+
+    [Fact]
+    public void Search_FirstElement()
+    {
+        var bs = new BinarySearch();
+        int[] arr = {1,2,3,4,5};
+        Assert.Equal(0, bs.Search(1, arr));
+    }
+
+    [Fact]
+    public void Search_LastElement()
+    {
+        var bs = new BinarySearch();
+        int[] arr = {1,2,3,4,5};
+        Assert.Equal(4, bs.Search(5, arr));
     }
 }
